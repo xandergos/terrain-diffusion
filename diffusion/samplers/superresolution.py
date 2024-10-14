27ee1df6f4b73d9e1a5bf1a619220e81239aa7e6
xandergos <28935064+xandergos@users.noreply.github.com>
2024-10-13 22:33:25 -0400

    Superresolution sampler

---
+++ diffusion/samplers/superresolution.py
@@ -0,0 +1,47 @@
+from functools import lru_cache
+
+from matplotlib import pyplot as plt
+import torch
+import torch.nn.functional as F
+from diffusion.samplers.tiled import TiledSampler
+
+
+def superresolution_sampler(samplers, upscaling_factors, encoders, postprocessor):
+    """
+    Inject the contextual network inputs into the samplers.
+    Args:
+        samplers (list[TiledSampler]): The samplers to inject the contextual network inputs into.
+        upscaling_factors (list[int]): The upscaling factors of the samplers. 
+            The first sampler does not have a parent so the list should be one element shorter than the number of samplers.
+    """
+    # To prevent modifying the list after the function is called
+    samplers = list(samplers)
+    for i, up in enumerate(upscaling_factors):
+        i += 1
+        
+        @lru_cache(maxsize=300)
+        def get_cond_img(tile_y, tile_x):
+            top, left, bottom, right = samplers[i].get_tile_bounds(tile_y, tile_x)
+            img_size = right - left
+            
+            # small_img is NOT encoded
+            img = samplers[i-1].get_region(top // up, left // up, bottom // up, right // up)
+            # upsample
+            img = torch.nn.functional.interpolate(img, (img_size, img_size), mode='bicubic', align_corners=False)
+            img = encoders[i].encode(img)
+            
+            return img
+        
+        def network_inputs(tiles_y, tiles_x, batch_idx):
+            return {'x': torch.stack([get_cond_img(tile_y, tile_x)[batch_idx] for tile_y, tile_x, batch_idx in zip(tiles_y, tiles_x, batch_idx)], dim=0)}
+        
+        def intermediate_postprocessor(tile_y, tile_x, img):
+            cond_img = get_cond_img(tile_y, tile_x)
+            return postprocessor(tile_y, tile_x, torch.cat([img[:, :1], cond_img[:, 1:]], dim=1))
+                
+        samplers[i].network_inputs = network_inputs
+        samplers[i].postprocessor = intermediate_postprocessor
+    return samplers[-1]
+
+if __name__ == "__main__":
+    pass
